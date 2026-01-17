29c7de015587b3842d128ff560232f3adf8702fc
Ethan Wong <87055387+ethanwongca@users.noreply.github.com>
2026-01-17 13:11:42 -0800

    Initial commit

---
+++ backend_gemini_to_audio/gemini_prompt_to_audio.py
@@ -0,0 +1,9 @@
+from google import genai
+if __name__ == "__main__"():
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+    )
+    print(response.text)
