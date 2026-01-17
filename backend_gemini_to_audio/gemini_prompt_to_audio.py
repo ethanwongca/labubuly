721253d0c5659e545ed0b4f82cebf6ef56c6e07f
Ethan Wong <87055387+ethanwongca@users.noreply.github.com>
2026-01-17 14:06:28 -0800

    Basic AI -> Audio Connection

--- backend_gemini_to_audio/gemini_prompt_to_audio.py
+++ backend_gemini_to_audio/gemini_prompt_to_audio.py
@@ -1,9 +1,29 @@
 from google import genai
-if __name__ == "__main__"():
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.play import play
+import os
+
+if __name__ == "__main__":
+    load_dotenv()
+    elevenlabs = ElevenLabs(
+    api_key=os.getenv("ELEVENLABS_API_KEY"),
+    )
+    
     # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-    client = genai.Client()
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+        model="gemini-3-flash-preview", contents="Slay Queen, how's you're day"
     )
-    print(response.text)
+    
+    audio = elevenlabs.text_to_dialogue.convert(
+        inputs=[
+            {
+                "text": f"{response.text}",
+                "voice_id": "JBFqnCBsd6RMkjVDRZzb"
+            }
+        ]
+    )
+
+    play(audio)
