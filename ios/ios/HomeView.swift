5fa45435558906ebb0fa9c961c9aacf18d59bf0a
Sophia Xu <hrxu@@students.cs.ubc.ca>
2026-01-17 22:44:17 -0800

    added endpoint fetch on buttons

--- ios/ios/HomeView.swift
+++ ios/ios/HomeView.swift
@@ -8,18 +8,35 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var service = VoiceService()
     var body: some View {
+        
         Menu("Voice Picker") {
-            Button("Default", action: sendVoice)
-            Button("Default", action: sendVoice)
-            Button("Default", action: sendVoice)
-            Button("Default", action: sendVoice)
-            Button("Default", action: sendVoice)
+            Button("Default") {
+                sendVoice(name: "Default")
+            }
+            Button("Breathy") {
+                sendVoice(name: "Breathy")
+            }
+            Button("Deep") {
+                sendVoice(name: "Deep")
+            }
+            Button("Whiny") {
+                sendVoice(name: "Whiny")
+            }
+            Button("Screech") {
+                sendVoice(name: "Screech")
+            }
         }
     }
-    func sendVoice() {
-        // call endpoint here
-        
+    func sendVoice(name: String) {
+        Task {
+            do {
+                try await service.selectVoice(named: name)
+            } catch {
+                print ("selection failed")
+            }
+        }
     }
 }
 
